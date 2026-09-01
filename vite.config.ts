82a0e049595a5d0ea25a753d52ca2e53bf379f0b
Beatrice Mutheu <beatiemutheu16@gmail.com>
2026-09-01 14:59:27 +0300

    update melt project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -13,9 +13,8 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+			// Deployed to Netlify via @sveltejs/adapter-netlify.
+			// See https://svelte.dev/docs/kit/adapter-netlify for more information.
 			adapter: adapter()
 		})
 	]
